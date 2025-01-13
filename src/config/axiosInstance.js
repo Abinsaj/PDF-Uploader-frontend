b1c880d22d2fa8144f30cf6f617bdbf2ada16e49
Abinsaj <148989575+Abinsaj@users.noreply.github.com>
2025-01-13 19:37:42 +0530

    latest

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "https://www.stagmensfashion.online"
+const url = "http://www.stagmensfashion.online"
 
 const axiosInsance = axios.create({
     baseURL: url,
